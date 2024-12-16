3ab624330db6220bde86c97993f6c856aee22f48
Gean ferrera <145035724+Dom-Garotom@users.noreply.github.com>
2024-12-16 11:11:16 -0300

    feat: criando um modal para gerenciar o scanner da câmera

---
+++ src/components/organismo/cameraModal/index.tsx
@@ -0,0 +1,41 @@
+import { View } from 'react-native'
+import { CameraView } from 'expo-camera'
+import { Modal } from 'react-native'
+import React, { useState } from 'react'
+
+
+import { Button } from '@/components/atomo/button'
+import useCoupon from '@/hooks/useCoupon'
+
+type Props = {
+    isVisibleCameraModal: boolean,
+    onClose : (action : boolean) => void
+}
+
+export default function CameraModal({ isVisibleCameraModal ,  onClose }: Props) {
+    const { CouponIsFetching, handleUseCoupon } = useCoupon()
+
+    return (
+        <Modal style={{ flex: 1 }} visible={isVisibleCameraModal}>
+            <CameraView
+                style={{ flex: 1 }}
+                facing='back'
+                onBarcodeScanned={({ data }) => {
+                    if (data) {
+                        // qrLock.current = true
+                        setTimeout(() => handleUseCoupon(data), 500)
+                    }
+                }}
+            />
+
+            <View style={{ position: 'absolute', bottom: 12, left: 20, right: 20 }} >
+                <Button
+                    onPress={() => onClose(false)}
+                    isLoading={CouponIsFetching}
+                >
+                    <Button.Title>Voltar</Button.Title>
+                </Button>
+            </View>
+        </Modal>
+    )
+}
